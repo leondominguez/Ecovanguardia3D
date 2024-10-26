8e58b3301b48498e904f7f99af3c41bc0d7a380e
leondominguez <leon.dominguez@correounivalle.edu.co>
2024-10-26 15:29:55 -0500

    Added turtle canvas login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,31 +1,37 @@
-import React, { useRef, useEffect } from 'react';
-import './Login.css';
+import TurtleCarey from '../../components/turtle/Turtle-carey';
+import "./Login.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+//import Octopus from "../../components/turtle/Turtle-carey";
+import { Suspense } from "react";
+
+// import { BoxGeometry } from 'three';
 
 const Login = () => {
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    
-    // Aquí va tu lógica para el modelo o efectos 3D
-    context.fillStyle = 'blue';
-    context.fillRect(10, 10, 100, 100);
-  }, []);
-
   return (
     <div className="login-container">
       <div className="login-section">
         <div className="logo">🌊</div>
-        <p>Por favor, cree una cuenta o inicie sesión con otro método para continuar.</p>
+        <p>
+          Por favor, cree una cuenta o inicie sesión con otro método para
+          continuar.
+        </p>
         <button>Iniciar sesión con Google</button>
         <a href="#">Inicia sesión con correo electrónico y contraseña.</a>
       </div>
       <div className="canvas-login">
-        <canvas ref={canvasRef} className="customCanvas" />
+        <Canvas className="customCanvas">
+          <Suspense fallback={null}>
+            <ambientLight intensity={10} />
+            <directionalLight position={[5, 5, 5]} intensity={10} />
+            <pointLight position={[10, 10, 10]} intensity={1} />
+            <TurtleCarey />
+            <OrbitControls />
+          </Suspense>
+        </Canvas>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
