f1a812b602d22d2913341fa547d1127b26b29bb3
Jose Castellanos <jmcastellanos153@misena.edu.co>
2024-10-30 19:10:27 -0500

    home con estilos

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,33 +1,49 @@
-import { Canvas } from "@react-three/fiber";
 import CircleHome from "../../components/circleHome/circleHome";
 import { Navbar } from "../../components/navbar/Navbar";
 import "./Home.css";
-import { Suspense } from "react";
-import { Ocean } from "../../components/ocean/Ocean";
-import Camera1 from "../../components/cameras/camera1";
-import { OrbitControls } from "@react-three/drei";
 
 function Home() {
   return (
     <>
-      <div className="home">
-        <Navbar></Navbar>
-        <div className="CopyHome">
-          <div className="title">
-            <h1 className="title-eco">ECO</h1>
-            <h1 className="title-vanguardia">VANGUARDIA</h1>
-          </div>
-
-          <h2 className="eslogan">Pequeños cambios,grandes impactos</h2>
-          <h3 className="invitacion">
-            Elige un tema y sumergete en la aventura ecovanguardista
-          </h3>
-        </div>
-        <Canvas className="customCanvas">
-          <Ocean/>   
-        </Canvas>
+  <div className="home">
+    <video
+      src="src/assets/videos/5607745-uhd_3840_2160_30fps.mp4"
+      autoPlay
+      loop
+      muted
+      className="background-video"
+    ></video>
+    <Navbar />
+    <div className="CopyHome">
+      <div className="title">
+        <h1 className="title-eco">ECO</h1>
+        <h1 className="title-vanguardia">VANGUARDIA</h1>
       </div>
-    </>
+
+      <h2 className="eslogan">Pequeños cambios, grandes impactos</h2>
+      <h3 className="invitacion">
+        Elige un tema y sumérgete en la aventura ecovanguardista
+      </h3>
+    </div>
+    <div className="horizontal-container">
+      <CircleHome
+        logo="src/assets/icons/sea-life-fish-svgrepo-com.svg"
+        slogan="Contaminación <br /> Del agua"
+      />
+      <CircleHome
+        logo="
+        src/assets/icons/faucet-svgrepo-com.svg"
+        slogan="Escasez <br /> Del agua"
+      />
+      <CircleHome
+        logo="
+        
+        src/assets/icons/sea-svgrepo-com.svg"
+        slogan="Acidificación <br /> de los Océanos"
+      />
+    </div>
+  </div>
+</>
   );
 }
 
