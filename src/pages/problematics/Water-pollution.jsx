1a0289940f3c957947b9603d2237fe7142d4dea9
Jose-Castellanos <castellanos.jose@correounivalle.edu.co>
2024-11-15 20:06:58 -0500

    agrego eventos de teclado y mouse

--- src/pages/problematics/Water-pollution.jsx
+++ src/pages/problematics/Water-pollution.jsx
@@ -1,34 +1,50 @@
 import { Suspense, useRef } from "react";
-import { Canvas,useFrame } from "@react-three/fiber";
-import { OrbitControls,PerspectiveCamera } from "@react-three/drei"; // Importa el modelo de contaminación del agua
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei"; // Importa el modelo de contaminación del agua
 import Buzo from "../../components/models-3d-component/buzo/Buzo";
-import WebGLSettings from '../../components/performance/WebGLSettings';
+import WebGLSettings from "../../components/performance/WebGLSettings";
 
 const WaterPollution = () => {
   const camera = useRef();
-  
+
   return (
     <Canvas
+      shadows
       camera={{ position: [0, 0, 15], fov: 50 }} // Ajusta la posición de la cámara según necesites
       style={{ width: "100%", height: "100%" }}
     >
       <PerspectiveCamera
         makeDefault
-        position={[0.2,0.6,3.6]}
+        position={[0.2, 0.6, 2]}
         fov={75} // Ajusta el campo de visión de la cámara
       />
-      <WebGLSettings pixelRatio={window.devicePixelRatio} powerPreference="high-performance" antialias={false} />
+      <WebGLSettings
+        pixelRatio={window.devicePixelRatio}
+        powerPreference="high-performance"
+        antialias={false}
+      />
       <Suspense>
-        <directionalLight position={[5, 3, -5]} intensity={2} />
-        <ambientLight  position={[0, 0, 0]} intensity={0.5} />
-        <pointLight position={[0, 1, 0]} intensity={1} />
-        <Buzo scale={1} />
-        <OrbitControls enableZoom={true} />
+        <directionalLight
+          position={[5, 3, -5]}
+          intensity={2}
+          castShadow
+          shadow-bias={-0.0005}
+        />
+        <ambientLight position={[0, 0, 0]} intensity={0.5} />
+        <pointLight
+          position={[0, 1, 0]}
+          intensity={1}
+          castShadow
+          shadow-bias={-0.0003} // Habilita castShadow
+          // shadow-mapSize-width={512}
+          // shadow-mapSize-height={512}
+        />
+        <Buzo scale={1}  />
+        <OrbitControls enableZoom={false} />
       </Suspense>
     </Canvas>
   );
 };
-
 
 // const LogCameraPosition = () => {
 //   useFrame(({ camera }) => {
